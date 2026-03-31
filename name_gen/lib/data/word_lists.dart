098e0466a43e83c419b4378b75325f371d3fa183
Munakotka <164500840+nkahkone@users.noreply.github.com>
2026-03-31 14:40:46 +0300

    create a list of basic cringe words

---
+++ name_gen/lib/data/word_lists.dart
@@ -0,0 +1,19 @@
+const List<String> adjectives = [
+  "Turbo",
+  "Silent",
+  "Dark",
+  "Epic",
+  "Crazy",
+  "Swift",
+  "Dapper"
+];
+
+const List<String> nouns = [
+  "Falcon",
+  "Tiger",
+  "Ninja",
+  "Wizard",
+  "Dragon",
+  "Shadow",
+  "Tort"
+];
